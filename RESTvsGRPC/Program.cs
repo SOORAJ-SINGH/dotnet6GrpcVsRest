9dfdc07eb9c95782a89e58547d88fdf31e7fb5f3
sooraj singh <soorajsingh@outlook.com>
2023-09-01 15:40:28 +0530

    Add project files.

---
+++ RESTvsGRPC/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace RESTvsGRPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start..");
+            Console.ReadKey();
+
+            BenchmarkRunner.Run<BenchmarkHarness>();
+            Console.ReadKey();
+        }
+    }
+}
